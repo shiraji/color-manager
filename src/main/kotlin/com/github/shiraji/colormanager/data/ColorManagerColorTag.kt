0101c69486e4ddfca85ecb0fe1e243570b132b33
shiraji <isogai.shiraji@gmail.com>
2016-09-04 11:14:30 +0900

    New Cell Rendering

--- src/main/kotlin/com/github/shiraji/colormanager/data/ColorManagerColorTag.kt
+++ src/main/kotlin/com/github/shiraji/colormanager/data/ColorManagerColorTag.kt
@@ -2,4 +2,4 @@ package com.github.shiraji.colormanager.data
 
 import com.intellij.psi.xml.XmlTag
 
-data class ColorManagerColorTag(val tag: XmlTag, val isInProject: Boolean)
+data class ColorManagerColorTag(val tag: XmlTag, val isInProject: Boolean, val fileName: String)
---
+++ src/main/kotlin/com/github/shiraji/colormanager/view/ColorManagerToolWindowCell.java
@@ -0,0 +1,10 @@
+package com.github.shiraji.colormanager.view;
+
+import javax.swing.*;
+
+public class ColorManagerToolWindowCell {
+    JPanel rootPanel;
+    JLabel colorCodeLabel;
+    JLabel fileNameLabel;
+    JLabel colorNameLabel;
+}
